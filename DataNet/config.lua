c3fb32d2ad681657a2abda9ccd1d7e62ec3c0a2d
Xavious <btothaw@gmail.com>
2022-01-14 16:00:00 -0500

    Adding Datanet
    Added Datanet and restructuring to see the contents, but also providing the .mpackage version

---
+++ DataNet/config.lua
@@ -0,0 +1,4 @@
+mpackage = [[DataNet]]
+author = [[Xavious]]
+description = [[A tabulated browser window for viewing datanet entries]]
+created = "2022-01-14T15:53:14-05:00"
---
+++ SpaceHud/config.lua
@@ -0,0 +1,3 @@
+mpackage = [[SpaceHud]]
+author = [[Xavious]]
+created = "2022-01-13T16:03:47-05:00"
