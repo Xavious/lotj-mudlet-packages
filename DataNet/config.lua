394a6b628e059336f3d8dc7b5a8c99eede6c7254
Xavious <btothaw@gmail.com>
2022-01-15 17:31:00 -0500

    A few more tweaks

--- DataNet/config.lua
+++ DataNet/config.lua
@@ -1,4 +1,4 @@
 mpackage = [[DataNet]]
 author = [[Xavious]]
 description = [[A tabulated browser window for viewing datanet entries]]
-created = "2022-01-15T16:50:43-05:00"
+created = "2022-01-15T17:29:49-05:00"
