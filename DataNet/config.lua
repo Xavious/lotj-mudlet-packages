a9a49bea9c7c17e356f9ca7eb5ddca1216aab7ca
Xavious <btothaw@gmail.com>
2022-01-18 14:22:20 -0800

    Updated starting position

--- DataNet/config.lua
+++ DataNet/config.lua
@@ -1,4 +1,4 @@
 mpackage = [[DataNet]]
 author = [[Xavious]]
 description = [[A tabulated browser window for viewing datanet entries]]
-created = "2022-01-16T20:30:51-05:00"
+created = "2022-01-18T13:02:28-08:00"
--- SpaceHud/config.lua
+++ SpaceHud/config.lua
@@ -1,4 +1,4 @@
 mpackage = [[SpaceHud]]
 author = [[Xavious]]
 description = [[An Eve Online inspired spreadsheet view of ships in space.]]
-created = "2022-01-16T20:30:43-05:00"
+created = "2022-01-18T12:59:51-08:00"
