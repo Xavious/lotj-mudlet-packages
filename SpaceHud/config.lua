245f72e6eb02f6ede2db4d3503f9670683467861
Xavious <btothaw@gmail.com>
2025-01-27 10:56:36 -0500

    Fixing locateship right-click option
    Due to changes in locateship, you only need to locate the ship name itself and not worry about single or double quotes. In fact, trying to quote the entire ship type and name doesn't work any longer, which broke this function.

--- SpaceHud/config.lua
+++ SpaceHud/config.lua
@@ -1,6 +1,4 @@
 mpackage = [[SpaceHud]]
 author = [[Xavious]]
-description = [[An Eve Online inspired spreadsheet view of ships in space.
-
-For help use command: "helpspace"]]
-created = "2023-03-11T23:48:04-05:00"
+description = [[An Eve Online inspired spreadsheet view of ships in space.]]
+created = "2025-01-27T10:49:36-05:00"
